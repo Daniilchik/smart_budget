aa8ef05761de4dbf26e8c172a9def09c67eaea5f
daniilchik <dprizva55@gmail.com>
2024-05-18 07:23:50 +0300

    hotfix

--- controller-module/src/main/java/org/parnassolutions/Controllers/OperationController.java
+++ controller-module/src/main/java/org/parnassolutions/Controllers/OperationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
--- controller-module/src/main/java/org/parnassolutions/Controllers/SavingController.java
+++ controller-module/src/main/java/org/parnassolutions/Controllers/SavingController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
