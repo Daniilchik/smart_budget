c2b5a37a6ea80cdf6e11adf100c939bc98dba12d
daniilchik <dprizva55@gmail.com>
2024-05-18 05:03:04 +0300

    Repositories added

---
+++ dao-module/src/main/java/org/parnassolutions/Repositories/OperationRepository.java
@@ -0,0 +1,9 @@
+package org.parnassolutions.Repositories;
+
+import org.parnassolutions.Entities.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Operation, Long> {
+}
